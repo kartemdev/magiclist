aada89c5527abaf81bd24fddd91aa25d4331307f
metrgram118 <ihctuyd@mail.ru>
2023-06-18 23:36:45 +0300

    update & add sass, css config for webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,13 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const isProduction = process?.env?.NODE_ENV === 'production';
 
 module.exports = {
-  mode: 'production',
+  mode:'development',
   entry: {
     index: path.resolve(__dirname, './src/index.tsx')
   },
   output: {
     path: path.resolve(__dirname, './build'),
-    filename: '[name].bundle.js'
+    filename: isProduction ? '[name].[hash].bundle.js' : '[name].bundle.js'
   },
   devServer: {
     static: {
@@ -15,6 +20,7 @@ module.exports = {
     },
     port: 8000,
     host: '127.0.0.1',
+    hot: true,
   },
   module: {
     rules: [
@@ -25,14 +31,35 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.(s(a|c)ss)$/,
+        exclude: /node_modules/,
+        use: [
+          isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader',
+          'sass-loader',
+        ]
+      },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.jsx', '.js'],
+    extensions: ['*', '.tsx', '.ts', '.jsx', '.js', '.scss'],
   },
+  plugins:[
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './public/index.html')
+    }),
+    new MiniCssExtractPlugin({
+      filename: isProduction ? '[name].[hash].css' : '[name].css',
+    }),
+  ],
   performance: {
     hints: false,
     maxEntrypointSize: 512000,
     maxAssetSize: 512000,
+  },
+  optimization: {
+    runtimeChunk: 'single'
   },
 }
