878216aa36cd1800e0337dcc77b9a0060cc0d4b1
kartemdev <ihctuyd@mail.ru>
2023-09-15 11:04:44 +0300

    feat: ML-32 update config for new host

--- src/shared/config/index.ts
+++ src/shared/config/index.ts
@@ -1,4 +1,7 @@
-export const API_BASE = 'https://wild-erin-moth-robe.cyclic.app/api/';
+export const API_BASE = 'https://api.magiclist.ru/api/';
+
+// API_BASE for dev
+// export const API_BASE = 'https://localhost:3000/api/';
 
 export const config = {
   API_BASE,
