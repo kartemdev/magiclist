54da8662ed13e984159cfd64c6659647f9a48be3
kartemdev <ihctuyd@mail.ru>
2023-09-21 12:27:30 +0300

    feat: ML-34 add config for deploy on prod

--- src/shared/config/index.ts
+++ src/shared/config/index.ts
@@ -1,7 +1,4 @@
-export const API_BASE = 'https://api.magiclist.ru/api/';
-
-// API_BASE for dev
-// export const API_BASE = 'https://localhost:3000/api/';
+export const API_BASE = process.env.API_HOST || 'http://localhost/api';
 
 export const config = {
   API_BASE,
