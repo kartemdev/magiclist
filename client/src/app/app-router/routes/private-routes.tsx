43390a6497420561d7723d372691bada56ed99b7
kartemdev <kartem8@yandex.ru>
2024-11-01 10:35:18 +0300

    fix: ML-78 add translated key for link & remove table test

--- client/src/app/app-router/routes/private-routes.tsx
+++ client/src/app/app-router/routes/private-routes.tsx
@@ -1,9 +1,7 @@
 import { lazy } from 'react';
-import { createColumnHelper } from '@tanstack/react-table';
 import { Navigate, Outlet, RouteObject, useLocation } from 'react-router-dom';
 
 import { useAppSelector } from '~shared/hooks';
-import { Table } from '~shared/ui';
 
 import { selectIsAuth } from '~services/auth';
 import { selectIsVerifiedUser } from '~services/user';
@@ -27,52 +25,9 @@ const Guard: React.FC = () => {
   return <Outlet />;
 };
 
-const tableData = [
-  {
-    email: 'arisha@milaya.ru',
-    address: 'raduga street',
-    age: 0.7,
-  },
-  {
-    email: 'julia@love.ru',
-    address: 'love street',
-    age: 21,
-  },
-  {
-    email: 'artyom@strong.ru',
-    address: 'fast street',
-    age: 22,
-  },
-];
-
-interface TableData {
-  age: number;
-  email: string;
-  address: string;
-}
-
-const getColumns = () => {
-  const columnHelper = createColumnHelper<TableData>();
-  return [
-    columnHelper.accessor('address', {
-      header: 'Адрес',
-    }),
-    columnHelper.accessor('email', {
-      header: 'Почта',
-    }),
-    columnHelper.accessor('age', {
-      header: 'Возраст',
-    }),
-  ];
-};
-
 const privateRoutes: RouteObject = {
   element: <Guard />,
   children: [
-    {
-      path: 'table',
-      element: <Table data={tableData} isMultipleSelect isCheckBoxSelect columns={getColumns()} />,
-    },
     {
       path: 'profile',
       element: <ProfilePage />,
--- client/src/modules/header/model/constants.model.ts
+++ client/src/modules/header/model/constants.model.ts
@@ -1,9 +1,5 @@
 export const AUTH_MAIN_LINKS = [
   {
-    to: '/table',
-    textContent: 'table',
-  },
-  {
     to: '/residues',
     textContent: 'head_link_residues',
   },
